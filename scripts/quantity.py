3c2060b8852b19c6c0a7c75b756784aa705a7960
Lucas Gabriel Pestana <lucasgabrielpestana03@gmail.com>
2024-02-22 15:55:58 -0300

    Add coments in quantity.py file: Describes step by step of quantity scenario

--- scripts/quantity.py
+++ scripts/quantity.py
@@ -5,22 +5,26 @@ from selenium.webdriver.common.by import By
 class QuantityScenario(BaseScenario):
 
   def getSelectedItemsAuto(self):
-
+    
     # Get only first three elements
     item_elems = self.driver.find_elements(by=By.CLASS_NAME, value="inventory_item")
     item_elems = item_elems[:3]
 
+    # Step 1: User clicks on "Add to cart" button that corresponds three selected products 
     for item_elem in item_elems:
 
       button_add_cart = item_elem.find_element(by=By.TAG_NAME, value="button")
       button_add_cart.click()
 
+    # Step 2: System adds products on cart
+    # Step 3: System adds quantity icon next to cart icon
     return item_elems
   
   def compareQuantityWithExpectedResult(self):
 
     getted_item_elems = self.getSelectedItemsAuto()
 
+    # WebElement that represents quantity icon
     expected_quantity_item = self.driver.find_element(by=By.CLASS_NAME, value="shopping_cart_badge")
 
     print("\033[1;34mGETTED \033[m|\033[1;32m EXPECTED\033[m")
